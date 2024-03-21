47a3ebe236bcd9c7ec9b7bbecc7255bde373331f
AH2857 <79082482+AH2857@users.noreply.github.com>
2024-03-21 16:50:51 +0000

    210324
    basic motion functions done, need to test them

--- Drone_Swarm/Assets/Unit Scripts/Motion Scripts/MotionFunctions.cs
+++ Drone_Swarm/Assets/Unit Scripts/Motion Scripts/MotionFunctions.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class MotionFunctions : MonoBehaviour
 {
     public Rigidbody Unit;
-    
-    
-    
-    // Rotate Function (default, modfy to make roll pitch and yaw)
+
+    // Unit "ghost" setup
+    Vector3 UnitApproxVelocity;                             // Represents the "measured" Velocity of the Unit, relative to its own axis
+
+    // Rot setup
+    Vector2 xMaxTurn        = new Vector2(45,45);           // X axis TurnRate (Max Positive, Max Negative)
+    Vector2 yMaxTurn        = new Vector2(45,45);           // Y axis TurnRate (Max Positive, Max Negative)
+    Vector2 zMaxTurn        = new Vector2(45,45);           // Z axis TurnRate (Max Positive, Max Negative)
+
+    // Thrust setup
+    Vector3 ThrustCurrent   = new Vector3(0,0,0);           // Thrust value in each direction (that will be applied per second using * Time.Deltatime)
+    Vector3 MaxThrustChange = new Vector3(50, 50, 50);      // Max value the change of thrust will be capped at (that will be applied per second using "* Time.Deltatime")
+    Vector3 MaxThrust       = new Vector3(200, 200, 200);   // Max value the thrust will be capped at (that will be applied per second using "* Time.Deltatime")
+
+    // Rotate Function (default, modify to make roll pitch and yaw)
     // Pass: Unit's odometry "ghost" Transform or actual transform, Unit's Transform, Target Angle, Maximum turn rate in positive and negative directions
-    bool TurnBasic(Transform UnitOdometry, Transform Unit, float TarAngle, float MaxPosTurn, float MaxNegTurn) 
+    bool Rot(Transform UnitPosition, Transform UnitTrans, float TargAngle, float MaxPosTurn, float MaxNegTurn) 
     {
-        // Check if at already at target angle by calculating difference between current and believed angle
-        // - if no output 0, and rotate as much as possible 
+        // Check if at already at target angle by calculating difference between current (approx) angle and target angle
+        // - if no output 0, and rotate as much as possible*  
         // - if yes output 1
 
-        float AngleDif = TarAngle - UnitOdometry.eulerAngles.x;         // Calc difference between current and believed angle 
+        // *Replace with more intelligent method later?
+
+        float AngleDif = TargAngle - UnitPosition.eulerAngles.x;        // Calc difference between current and believed angle 
         if ((AngleDif < 1) && (AngleDif > -1))                          // check if outside acceptable range
         {
             return true;
@@ -26,42 +39,84 @@ public class MotionFunctions : MonoBehaviour
             float TurnAngle = AngleDif;                                                 // Calculate TurnAngle
             if (TurnAngle > MaxPosTurn) { TurnAngle = MaxPosTurn; }                     // Cap turn at Max positive turn
             if (TurnAngle < MaxNegTurn) { TurnAngle = MaxNegTurn; }                     // Cap turn at max negative turn
-            Unit.RotateAround(Unit.position, Unit.right, TurnAngle * Time.deltaTime);   // Execute turn at value required (cap at max rates of turn)
+            UnitTrans.RotateAround(UnitTrans.position, UnitTrans.right, TurnAngle * Time.deltaTime);   // Execute turn at value required (cap at max rates of turn)
+            return false;
+        }
+    }
+    bool ThrustToSpeed(float TargSpeed, float ApproxVel, float MaxThrustChange, float MaxThrust, ref float ThrustCurrent)
+    {
+        // Check if already at target speed by calculating difference between current speed and target speed
+        // - if no output 0 and increase thrust as much as possible*
+        // - if yes output 1 and keep thrust the same
+
+        float SpeedDif = TargSpeed - ApproxVel;
+        if ((SpeedDif < 1) && (SpeedDif > -1))
+        {
+            // Dont modify the current thrust
+            return true;
+        }
+        else
+        {
+            float ThrustAdjust = (SpeedDif * 10);                                       // calculate thrust adjustment, adjust the 
+            if (ThrustAdjust > MaxThrustChange) { ThrustAdjust = MaxThrustChange; };    // Cap max thrust increase
+            if (ThrustAdjust < MaxThrustChange) { ThrustAdjust = -MaxThrustChange; };   // Cap max thrust increase
+            ThrustCurrent += (ThrustAdjust);                                            // adjust current thrust
+            if (ThrustCurrent > MaxThrust) { ThrustCurrent = MaxThrust; };              // Cap max thrust
             return false;
         }
     }
 
-
-    /*
-    // Change main thrust 
-    // Pass: Rigidbody of Engine, Target Force, Maximum Force, Minimum Force)
-    bool UpForceBasic(Rigidbody Engine, float TarForce, float MaxForce, float MinForce)
-    {
-        float Difference = TarForce - c
-        
-    }
-    */
-
-    //float MaxForce, float MinForce,
-
-    // apply thrust
-    // Pass: Rigidbody of Engine or Unit, Force to apply, direction (vector3,up / vector3.forward)
-    void Force(Rigidbody Engine, float Force, Vector3 direction)
-    {
-        //Engine.AddForce(direction * Force);
-        Engine.AddRelativeForce(direction * Force);
+    bool Roll(Transform A, Transform B, float C){
+        return Rot(A, B, C, zMaxTurn.x, zMaxTurn.y);
     }
 
+    bool Pitch(Transform A, Transform B, float C)
+    {
+        return Rot(A, B, C, xMaxTurn.x, xMaxTurn.y);
+    }
+
+    bool Yaw(Transform A, Transform B, float C)
+    {
+        return Rot(A, B, C, yMaxTurn.x, yMaxTurn.y);
+    }
+    
+    // Adjust thrust to achieve target speed for Z axis 
+    // Pass: Target Speed
+    bool ZThrustToSpeed(float zSpeed)
+    {
+        return ThrustToSpeed(zSpeed, UnitApproxVelocity.z, MaxThrustChange.z, MaxThrust.z, ref ThrustCurrent.z);
+    }
+
+    // Adjust thrust to achieve target speed for Y axis 
+    // Pass: Target Speed
+    bool YThrustToSpeed(float ySpeed)
+    {
+        return ThrustToSpeed(ySpeed, UnitApproxVelocity.y, MaxThrustChange.y, MaxThrust.z, ref ThrustCurrent.z);
+    }
+
+    // Adjust thrust to achieve target speed for X axis 
+    // Pass: Target Speed
+    bool XThrustToSpeed(float xSpeed)
+    {
+        return ThrustToSpeed(xSpeed, UnitApproxVelocity.x, MaxThrustChange.x, MaxThrust.x, ref ThrustCurrent.z);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Force(Unit, 1000, Vector3.forward); // Must run this function every update
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Update "ghost" unit values by measureing them (position, orientation and speed)
+        UnitApproxVelocity = Unit.velocity; // replace with method of working out velocity relative to own axes
+
+
         
+        
+        // Thrust Forces are applied
+        Unit.AddRelativeForce(Vector3.forward * ThrustCurrent.z * Time.deltaTime);
     }
 }
