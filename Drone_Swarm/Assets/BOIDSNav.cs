4061c9d96cd095f1f6deb4e774e5026b303f9d01
AH2857 <79082482+AH2857@users.noreply.github.com>
2024-04-12 14:57:19 +0100

    120424
    Object tracker working
    started on Nav script, trying to send array of obj data to nav script from obj tracker script

---
+++ Drone_Swarm/Assets/BOIDSNav.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOIDSNav : MonoBehaviour
+{
+    Vector3 navVector;
+    GameObject ObjTrackChild;
+
+    // Functions updating navVector
+
+    // Seperation
+    //Vector3 Seperation()
+
+    // Alignment
+
+    // Cohesion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // refresh tracked object data array 
+
+        
+
+        // nav recalculation timer
+    }
+}
--- Drone_Swarm/Assets/ObjectTracker2.cs
+++ Drone_Swarm/Assets/ObjectTracker2.cs
@@ -6,12 +6,26 @@ public class ObjectTracker2 : MonoBehaviour
 {
     public bool Terminal = false;
     public float range;
+    LayerMask RaycastIgnores;
 
     void spheresetup()
     {
         SphereCollider SearchSphere = gameObject.AddComponent<SphereCollider>();    // Add sphere collider component
         SearchSphere.radius = range;                                                // Scale sphere collider to represent scan range    
         SearchSphere.isTrigger = true;
+
+        // prevent other unit's raycasts from detecting this unit's sphere collider
+        // this is achieved by putting it ona new layer and ignoring that layer in raycasts
+        int ObjectTrackerLayer = 6;
+        RaycastIgnores = ~LayerMask.GetMask("ObjectTracker");
+        //SearchSphere.excludeLayers(RaycastIgnores);       // Unity version 23 feature
+        Physics.IgnoreLayerCollision(ObjectTrackerLayer, ObjectTrackerLayer);
+
+        
+        
+        //RaycastIgnores = (1 << ObjectTrackerLayer); // put sphere on Object Tracker layer, layer 6 
+        //RaycastIgnores = ~(LayerMask.GetMask("ObjectTracker"));
+
     }
 
     // --- Detecting Objects ---
@@ -21,9 +35,11 @@ public class ObjectTracker2 : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if(NumDetected <= MaxDetectObj)                                 // checks if space to save new detcted object's position
+        if(NumDetected < MaxDetectObj)                               // checks if space to save new detcted object's position
         {
-            Debug.Log("Detected object");
+            //Debug.Log("Detected object");
+            //Debug.Log(NumDetected);
+
             DetectedObjPos[NumDetected] = other.transform.position;     // Saves detected object's position to array
             NumDetected++;                                              // increment number of saved detected objects
         }
@@ -34,7 +50,7 @@ public class ObjectTracker2 : MonoBehaviour
     public const int MaxSavedObj = 10;
     int NumSavedObj = 0;
 
-    struct ObjData                          // storing data returned from raycast in struct
+    public struct ObjData                          // storing data returned from raycast in struct
     {
         public bool Locked;
         public float objDistance;
@@ -44,6 +60,7 @@ public class ObjectTracker2 : MonoBehaviour
     }
 
     ObjData[] savedObj = new ObjData[MaxSavedObj];      // Array for storing data of objects that have been searched for
+    public ObjData[] navObj = new ObjData[MaxSavedObj];        // Output Array, updated at end of frame that Navigation scripts use
 
     void saveObj(RaycastHit data)
     {
@@ -52,8 +69,9 @@ public class ObjectTracker2 : MonoBehaviour
         savedObj[NumSavedObj].objPosition = data.point;                                        // Obj Position
         savedObj[NumSavedObj].objDirVector = data.transform.eulerAngles;                       // obj Rotation
         savedObj[NumSavedObj].objTag = data.collider.tag;                                      // obj Tag
-
+        
         NumSavedObj++;  // increment number of saved objects
+        //Debug.Log(NumSavedObj);
     }
 
     // Start is called before the first frame update
@@ -65,35 +83,54 @@ public class ObjectTracker2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //SearchSphere.radius = range;
+
         for(int j = 0; j < MaxSavedObj; j++) { savedObj[j].Locked = true; }  // Set each part of saved obj array as Locked, until it is overwritten
         NumSavedObj = 0;
         if (Terminal){ Debug.Log("1"); }
+
+        // update with a timer later for performance, only searching for objects after a certain delay
+
         for(int i = 0; i < NumDetected; i++)    // starting at beginning of detected objects array, searches for all the detected objects
         {
             if (Terminal) { Debug.Log("2"); }
-            Vector3 VecTowObj = DetectedObjPos[i] - gameObject.transform.position;                  // vector towards object transform
+            Vector3 VecTowObj = DetectedObjPos[i] - transform.position;                  // vector towards object transform
             RaycastHit hit;                                                                         // raycast hit data
             
             Vector3 RaycastDir = Vector3.Normalize(VecTowObj) * range;                              // Create vector in direction of target, with length equal detection range
             Debug.DrawRay(transform.position, RaycastDir, Color.blue);
-
-            if (Physics.Raycast(transform.position, RaycastDir, out hit) && (NumSavedObj < MaxSavedObj))   // if detects the object, and there is room in SavedObjects array
+            
+            if (Physics.Raycast(transform.position, VecTowObj, out hit, range, RaycastIgnores) && (NumSavedObj < MaxSavedObj))   // if detects the object, and there is room in SavedObjects array
             {
+                Debug.Log("hit");
                 saveObj(hit);   // save object data
                 if (Terminal) { Debug.Log("3"); }
             }
         }
 
+        //Debug.Log("NumSavedObj:");
+        //Debug.Log(NumSavedObj);
         
+        // Display all stored contacts
         for (int h = 0; h < NumSavedObj; h++)
         {
-            if (Terminal) { Debug.Log("4"); }
+            Debug.Log(h);
+            Debug.Log(savedObj[h].Locked);
+            if (savedObj[h].Locked == false) {
+                if (Terminal) { Debug.Log("4"); }
 
-            Vector3 objraydir = savedObj[h].objDirVector * savedObj[h].objDistance; 
-            Ray objray = new Ray(transform.position, objraydir);                    // create ray described by stored data
-            Debug.DrawRay(transform.position, objray.direction, Color.red);
+                Vector3 objraydir = savedObj[h].objPosition - transform.position;
+                //Vector3 objraydir = savedObj[h].objDirVector * savedObj[h].objDistance;
+                //Debug.Log(savedObj[h].objDistance);
+                //Ray objray = new Ray(transform.position, objraydir);                    // create ray described by stored data
+                //Debug.DrawRay(transform.position, objray.direction, Color.red);
+                Debug.DrawRay(transform.position, objraydir, Color.red);
+            }
+            
+            
         }
-        
+
+        navObj = savedObj; // Publish Array of data for Navigation to use
 
 
 
