23cbb03a5f6320a2e701951f2894a76a4a662aa4
AH2857 <79082482+AH2857@users.noreply.github.com>
2024-04-22 14:59:06 +0100

    220424b

---
+++ Drone_Swarm/Assets/Aero_Motion.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aero_Motion : MonoBehaviour
+{
+    // Implement turning and acceleration
+        // Max speed (higher than speed at which drag equals acceleration)
+        // Max Acceleration force
+    // Implement Drag (this applies a force that opposes the direction of travel, proportional to the final speed after acceleration)
+    // Implent Lift (this applies a lift force in the unit's local y axis proportional to the unit's speed in the it's local forward direction
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get heading vector from nav script
+
+
+    }
+}
